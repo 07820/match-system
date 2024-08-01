e0ab30b353596e3dc80270b5051370742d68518f
Yanqi Xu <120089324+07820@users.noreply.github.com>
2024-08-01 13:15:54 +0800

    Integrate vant UI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
+import {Button} from 'vant';
 
-createApp(App).mount('#app')
+const app=createApp(App);
+app.use(Button);
+app.mount('#app')
+
+
+
