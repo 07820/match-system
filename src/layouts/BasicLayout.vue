76ecc3945322bda872596857aef7f2a2a1165468
Yanqi Xu <120089324+07820@users.noreply.github.com>
2024-08-01 14:02:15 +0800

    test navbar

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,33 @@
+<!--<script setup lang="ts">-->
+
+<!--</script>-->
+
+<template>
+  <van-nav-bar
+      title="title"
+      left-text="back"
+      right-text="button"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  />
+
+</template>
+
+
+
+
+<script setup>
+
+const onClickLeft = () => alert('返回');
+const onClickRight = () => alert('按钮');
+</script>
+
+
+
+
+<style scoped>
+
+
+
+</style>
