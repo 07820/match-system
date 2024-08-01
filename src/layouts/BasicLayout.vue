6f9f348f53260a9aa31f93256afde68db3dbd59c
Yanqi Xu <120089324+07820@users.noreply.github.com>
2024-08-01 14:48:11 +0800

    add tabBar

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -6,11 +6,27 @@
   <van-nav-bar
       title="title"
       left-text="back"
-      right-text="button"
+
       left-arrow
       @click-left="onClickLeft"
       @click-right="onClickRight"
-  />
+  >
+
+<!--    插槽语法-->
+  <template #right>
+    <van-icon name="search" size="18" />
+  </template>
+    </van-nav-bar>
+
+
+
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item icon="home-o" name="index">main</van-tabbar-item>
+    <van-tabbar-item icon="search" name="team">team</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" name="user">user</van-tabbar-item>
+
+  </van-tabbar>
+
 
 </template>
 
@@ -19,8 +35,14 @@
 
 <script setup>
 
+import {ref} from "vue";
+import {showToast} from "vant";
+
 const onClickLeft = () => alert('返回');
 const onClickRight = () => alert('按钮');
+
+const active = ref("index");
+const onChange = (index) => showToast(`标签 ${index}`);
 </script>
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-import {Button} from 'vant';
+import {Button, Icon} from 'vant';
 
 const app=createApp(App);
 app.use(Button);
+app.use(Icon)
+
+
 app.mount('#app')
 
 
